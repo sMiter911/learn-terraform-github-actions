d4174ce88ba0a042132e073655c52ca43f890b3b
Wandu <31390092+sMiter911@users.noreply.github.com>
2024-01-31 19:44:34 +0200

    Provider Fix

--- sys_vpc_create.tf
+++ sys_vpc_create.tf
@@ -1,9 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.52.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
 provider "aws" {
   alias  = "vpc"
   region = "us-west-2" # Change this to your desired AWS region
 }
 
 resource "aws_vpc" "my_vpc" {
+  provider             = aws.vpc # Use the provider alias here
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
@@ -14,6 +29,7 @@ resource "aws_vpc" "my_vpc" {
 }
 
 resource "aws_subnet" "my_subnet" {
+  provider          = aws.vpc # Use the provider alias here
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-west-2" # Change this to your desired availability zone
