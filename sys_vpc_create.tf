5cb9bd898878d38c176efc3f11474b977d70cc9a
Wandu <31390092+sMiter911@users.noreply.github.com>
2024-01-31 19:58:57 +0200

    Update sys_vpc_create.tf

--- sys_vpc_create.tf
+++ sys_vpc_create.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.52.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
-    }
-  }
-  required_version = ">= 1.1.0"
-}
-
 provider "aws" {
   alias      = "vpc"
   region     = "us-west-2"
