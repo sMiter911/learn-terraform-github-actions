758d340918c2d044c15cf7b328f9175107de6490
Wandu <31390092+sMiter911@users.noreply.github.com>
2024-02-01 07:06:35 +0200

    WIP: file deletion,var update

--- sys_vars.tfvars
+++
@@ -1,2 +0,0 @@
-access_key = "AKIATG6AXPR573WVUKDI"
-secret_key = "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"
--- sys_vpc_create.tf
+++ sys_vpc_create.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   alias      = "vpc"
   region     = "us-west-2"
-  access_key = var.access_key
-  secret_key = var.secret_key
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 resource "aws_vpc" "my_vpc" {
